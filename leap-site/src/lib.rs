cce401cc0a8b737e7566640ee12a294961a23389
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-05-25 23:14:05 +0200

    chore: Improve documentation leap_site

--- leap-site/src/lib.rs
+++ leap-site/src/lib.rs
@@ -1,3 +1,11 @@
+//! The `leap-site` crate provides the core components and pages for the Leap website.
+//!
+//! It includes:
+//! - Application logic and state management in [`crate::app`] and [`crate::context`].
+//! - Page components in [`crate::pages`].
+//!
+//! This crate is designed to be used with [Yew](https://yew.rs/).
+
 pub mod app;
 pub mod context;
 pub mod pages;

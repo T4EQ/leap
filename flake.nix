2450866413224b2682bd67586f8f79df851a8e3f
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-06-25 22:08:58 +0200

    update wasm-bindgen-cli to 0.2.126

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
                     nativeBuildInputs = with pkgsWithRustOverlay; [
                       (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
                       trunk
-                      config.packages.wasm-bindgen-cli_0_2_125
+                      config.packages.wasm-bindgen-cli_0_2_126
                       dart-sass
                       cargo-watch
                       cargo-deny
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,16 +4,16 @@
     { pkgs, ... }:
     {
       packages = {
-        wasm-bindgen-cli_0_2_125 = pkgs.rustPlatform.buildRustPackage rec {
+        wasm-bindgen-cli_0_2_126 = pkgs.rustPlatform.buildRustPackage rec {
         pname = "wasm-bindgen-cli";
-        version = "0.2.125";
+        version = "0.2.126";
 
         src = pkgs.fetchCrate {
           inherit pname version;
-          hash = "sha256-zRawtjxMOdTMX+mZaiNR3YYfTiZJhf9qj7kXSSeMxrc=";
+          hash = "sha256-H6Is3fiZVxZCfOMWK5dWMSrtn50VGv0sfdnsT+cTtyk=";
         };
 
-        cargoHash = "sha256-aZCfgR23Qb0Pn4Mm4ToMtuuRQqSJjXCR9li/VvP5CTM=";
+        cargoHash = "sha256-VucqkXbCi4qtQzY/HrXiDnbSURsagPsdNVMn1Tw3UiY=";
 
         nativeBuildInputs = [ pkgs.pkg-config ];
         buildInputs = with pkgs; [ openssl ];

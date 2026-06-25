5a4a5a8cd7df2f463fd6e18c0d3ea0b19bb9721d
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-06-25 22:08:58 +0200

    update wasm-bindgen-cli to 0.2.126

--- leap-provision-site/default.nix
+++ leap-provision-site/default.nix
@@ -19,7 +19,7 @@
 
           nativeBuildInputs = with pkgs; [
             trunk
-            config.packages.wasm-bindgen-cli_0_2_125
+            config.packages.wasm-bindgen-cli_0_2_126
             dart-sass
             lld
           ];

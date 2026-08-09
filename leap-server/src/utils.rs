162153588fcef90a139a50fa4c8dd99e6061c354
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-08-09 15:55:28 +0200

    chore: Mock timesync features if we do not run on Linux

--- leap-server/src/utils.rs
+++ leap-server/src/utils.rs
@@ -12,18 +12,26 @@ use std::time::Duration;
 /// Returns `Ok(true)` if synchronized, `Ok(false)` if not, or an error if `timedatectl` does not
 /// exit with a zero status code.
 pub async fn check_timesync() -> anyhow::Result<bool> {
-    let output = tokio::process::Command::new("timedatectl")
-        .arg("show")
-        .arg("-P")
-        .arg("NTPSynchronized")
-        .output()
-        .await?;
-    if !output.status.success() {
-        tracing::error!("Failure checking time synchronization {output:?}");
-        anyhow::bail!("Failure checking time synchronization {output:?}");
-    }
+    std::cfg_select! {
+        target_os = "linux" => {
+            let output = tokio::process::Command::new("timedatectl")
+                .arg("show")
+                .arg("-P")
+                .arg("NTPSynchronized")
+                .output()
+                .await?;
+            if !output.status.success() {
+                tracing::error!("Failure checking time synchronization {output:?}");
+                anyhow::bail!("Failure checking time synchronization {output:?}");
+            }
 
-    Ok(output.stdout == b"yes\n")
+            Ok(output.stdout == b"yes\n")
+        }
+        _ => {
+            // We don't support time sync in other operating systems
+            Ok(false)
+        }
+    }
 }
 
 /// Waits for the system time to be synchronized with NTP.
@@ -34,13 +42,21 @@ pub async fn check_timesync() -> anyhow::Result<bool> {
 ///
 /// Returns an error if the timeout is reached before synchronization is confirmed.
 pub async fn wait_timesync(timeout: Duration) -> anyhow::Result<()> {
-    let start = std::time::Instant::now();
-    while std::time::Instant::now() - start < timeout {
-        if check_timesync().await? {
-            return Ok(());
-        }
-        tokio::time::sleep(Duration::from_millis(100)).await;
-    }
+    std::cfg_select! {
+        target_os = "linux" => {
+            let start = std::time::Instant::now();
+            while std::time::Instant::now() - start < timeout {
+                if check_timesync().await? {
+                    return Ok(());
+                }
+                tokio::time::sleep(Duration::from_millis(100)).await;
+            }
 
-    anyhow::bail!("Timeout while waiting for time synchronization");
+            anyhow::bail!("Timeout while waiting for time synchronization");
+        }
+        _ => {
+            let _ = timeout;
+            anyhow::bail!("Time sync is not supported in this operating system");
+        }
+    }
 }
